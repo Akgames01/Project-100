7c9ac7f442e7d1eab18a82ed35819a8f69656ca8
Akgames01 <arnav_kumars@rocketmail.com>
2020-04-03 14:01:14 +0530

    Project 100

---
+++ src/Project100/Main/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    Name = models.CharField(max_length=200,blank=False,null=False)
+    UserID = models.CharField(max_length=100,blank=False,null=False)
+    Password = models.CharField(max_length=100,blank=False,null=False)
+    DOB = models.DateField(blank=False,null=False)
+    email = models.EmailField(default='Example@gmail.com',blank=False,null=False)
+    Phone = models.CharField(default=' ',max_length=10,blank=False,null=False)
+    PlaceofBirth = models.TextField(default='',blank=True,null=True)
