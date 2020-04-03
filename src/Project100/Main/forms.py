1180a344831932294b96b4a7c09391b1afaa9ffc
Akgames01 <arnav_kumars@rocketmail.com>
2020-04-03 14:52:35 +0530

    Project 100

--- src/Project100/Main/forms.py
+++ src/Project100/Main/forms.py
@@ -7,25 +7,34 @@ class UserForm(forms.ModelForm):
     Name = forms.CharField(widget=forms.TextInput(attrs={
         'placeholder': 'Enter your Name',
         'id': 'Name1',
+        'class':"form-control",
     }))
     UserID = forms.CharField(widget=forms.TextInput(attrs={
         'placeholder': 'UserId example:some1%43',
         'id': 'ID',
+        'class':"form-control",
     }))
     Password = forms.CharField(widget=forms.PasswordInput(attrs={
         'placeholder': '*********',
         'id': 'pass',
+        'class':"form-control",
     }))
     DOB = forms.DateField(widget=forms.DateInput(attrs={
         'placeholder': 'MM-DD-YYYY',
         'id': 'DOB',
+        'class':"form-control",
     }))
     email = forms.EmailField(widget=forms.EmailInput(attrs={
         'placeholder': 'some123@example.com',
         'id': 'email',
+        'class':"form-control",
     }))
-    Phone = forms.CharField()
-    PlaceofBirth = forms.CharField(required=False)
+    Phone = forms.CharField(widget = forms.TextInput(attrs={
+        "class":"form-control"
+    }))
+    PlaceofBirth = forms.CharField(required=False,widget = forms.TextInput(attrs={
+        "class":"form-control"
+    }))
     class Meta:
         model = User
         fields = [
